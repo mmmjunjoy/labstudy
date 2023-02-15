f8e1983abde8e26a9813a66563acb66d092fc1f7
mmmjunjoy <sjbssk0809@gmail.com>
2023-02-16 00:53:22 +0900

    feat:vue_basic

---
+++ vue_study/moz-todo-vue/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1>My To-Do List</h1>
+    <ul>
+      <li>
+        <to-do-item label="My ToDo Item" :done="true"></to-do-item>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import ToDoItem from './components/ToDoItem.vue';
+
+  export default {
+    name: 'app',
+    components: {
+      ToDoItem
+    }
+  };
+
+  
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
